d1071a2e0499af57607dbc05970de539fd203dd8
yiwanjun <yiwanjun@daixiaomi.com>
2019-07-18 16:17:36 +0800

    还款中心UI完成

--- Demos/RepayCenterScreen.js
+++ Demos/RepayCenterScreen.js
@@ -12,31 +12,93 @@ import {
 } from 'react-native';
 import {Ionicons} from '@expo/vector-icons'
 
-class VerticalText extends React.Component {
+class SectionHeader extends React.Component {
     render() {
+        
         return (
-            <View style={CellStyles.con}>
+            <View style={SectionHeaderStyles.con}>
+                <Text style={SectionHeaderStyles.text}>{this.props.title}</Text>
+            </View>
+        );
+    }
+}
+
+class SectionFooter extends React.Component {
+    render() {
+        return (<View
+            style={{
+            backgroundColor: '#fff',
+            height: 20
+        }}/>);
+    }
+}
+
+const SectionHeaderStyles = {
+    con: {
+        flex: 1,
+        marginTop: 19,
+        height: 30,
+        flexDirection: 'column',
+        justifyContent: 'center'
+    },
+    text: {
+        fontSize: 16,
+        //不写下面两个，Android系统上文字会偏下
+        includeFontPadding: false,
+        textAlignVertical: 'center',
+        fontWeight: 'bold',
+       
+        color: '#333333',
+    }
+};
+
+class RowText extends React.Component {
+    render() {
+        let questionImg = null;
+        if (this.props.hasQuestion) {
+            questionImg = <Image
+                style={CellStyles.repayPlanCellLeftImage}
+                source={require('../assets/images/repay_btn_explain.png')}/>;
+        }
+        return (
+            <View
+                style={[
+                CellStyles.con, CellStyles.conJustifyContent(this.props.isRight)
+            ]}>
                 <Text
                     style={[
-                    CellStyles.text, CellStyles.textAlign(this.props.textAlign),
-                    CellStyles.textColor(this.props.textColor)
+                    CellStyles.text, CellStyles.textColor(this.props.textColor)
                 ]}>{this.props.content}</Text>
+
+                {questionImg}
             </View>
+
         );
     };
 }
 
 class Cell extends React.Component {
+
     render() {
+        let img = null;
+        if (this.props.hasArr) {
+            img = <Image
+                style={CellStyles.repayPlanCellRightArrow}
+                source={require('../assets/images/gray_right_arrow.png')}/>;
+        }
+
         return (
             <View style={CellStyles.cell}>
                 <TouchableHighlight >
                     <View style={CellStyles.repayPlanCell}>
-                        <VerticalText content={this.props.leftText} textColor='#666666'></VerticalText>
-                        <VerticalText content={this.props.rightText} textAlign='right'></VerticalText>
-                        <Image
-                            style={CellStyles.repayPlanCellRightArrow}
-                            source={require('../assets/images/gray_right_arrow.png')}></Image>
+                        <RowText
+                            content={this.props.leftText}
+                            textColor='#666666'
+                            hasQuestion={this.props.hasQuestion}/>
+
+                        <RowText content={this.props.rightText} isRight='true'/>
+                        {img}
+
                     </View>
                 </TouchableHighlight>
             </View>
@@ -45,45 +107,45 @@ class Cell extends React.Component {
 }
 
 const CellStyles = {
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
     con: {
         flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        // backgroundColor: '#123456'
+        flexDirection: 'row'
+    },
+    conJustifyContent(arg) {
+        let justifyContent = 'flex-start';
+        if (arg) {
+            justifyContent = 'flex-end';
+        }
+        return {justifyContent: justifyContent};
     },
     cell: {
-        //   flex: 1,
-        height: 28,
-        //   width:50,
+        marginTop: 4,
+        height: 24,
         backgroundColor: '#fff',
-        flexDirection: 'column',
-        justifyContent: 'center'
+        flexDirection: 'column'
     },
     repayPlanCell: {
         height: 20,
         flexDirection: 'row',
-        backgroundColor: '#F5FCFF'
+        backgroundColor: '#fff'
     },
     repayPlanCellRightArrow: {
-        //   justifyContent:'center',
         marginTop: 2,
         marginLeft: 4,
         width: 7,
         height: 14
     },
+    repayPlanCellLeftImage: {},
     text: {
         fontSize: 14,
         backgroundColor: '#fff',
-        // textAlign: 'right', 不写下面两个，Android系统上文字会偏下
+        // 不写下面两个，Android系统上文字会偏下
         includeFontPadding: false,
         textAlignVertical: 'center'
-    },
-    textAlign(arg) {
-        let algin = 'left';
-        if (arg) {
-            algin = arg;
-        }
-        return {textAlign: algin};
     },
     textColor(arg) {
         let color = '#333333';
@@ -112,7 +174,7 @@ export default class RepayCenterScreen extends React.Component {
             <SafeAreaView
                 style={{
                 flex: 1,
-                backgroundColor: '#fff'
+                backgroundColor: '#f5f5f5'
             }}>
                 <ScrollView contentInsetAdjustmentBehavior="never">
                     <View
@@ -142,24 +204,14 @@ export default class RepayCenterScreen extends React.Component {
                             }}
                                 onChangeText={(text) => this.setState({text})}
                                 value={this.state.text}/>
-                            <Text
-                                style={{
-                                fontWeight: 'bold',
-                                fontSize: 16,
-                                marginTop: 28,
-                                height: 22,
-                                color: '#333333'
-                            }}>待还详情</Text>
+                           
+                            <SectionHeader title='待还详情'/>
 
                             <View >
-                                <Cell leftText='还款计划(偿还2期)' rightText='2313.23元'/>
+                                <Cell leftText='还款计划(偿还2期)' rightText='2313.23元' hasArr='true'/>
                                 <Cell leftText='部分还款' rightText='-200.23元'/>
-                                <Cell leftText='逾期费用' rightText='+46.2元'/>
-                                <View
-                                    style={{
-                                    backgroundColor: '#fff',
-                                    height: 23
-                                }}/>
+                                <Cell leftText='逾期费用' rightText='+46.2元' hasQuestion='true'/>
+                                <SectionFooter />
                             </View>
 
                             {/* <Button onPress={() => this.props.navigation.push('RepayCenter')} title='点我' style={{marginTop: 140}}/> */}
@@ -167,24 +219,19 @@ export default class RepayCenterScreen extends React.Component {
                         </View>
                         <View
                             style={{
-                            backgroundColor: '#e6e6e6',
+                            backgroundColor: '#f5f5f5',
                             height: 8
                         }}/>
                         <View
                             style={{
                             backgroundColor: '#fff',
-                            marginLeft: 15
+                            marginLeft: 15,
+                            marginRight: 15
                         }}>
-                            <Text
-                                style={{
-                                fontWeight: 'bold',
-                                fontSize: 16,
-                                marginTop: 16,
-                                height: 22,
-                                color: '#333333'
-                            }}>优惠券详情</Text>
-                            <Cell leftText='还款现金券' rightText='-5元'/>
+                            <SectionHeader title='优惠券详情'/>
+                            <Cell leftText='还款现金券' rightText='-5元' hasArr='true'/>
                             <Cell leftText='借款免息券(每期结清生效)' rightText='-13.5元'/>
+                            <SectionFooter />
                         </View>
                         <View
                             style={{
@@ -196,21 +243,26 @@ export default class RepayCenterScreen extends React.Component {
                 </ScrollView>
                 <View
                     style={{
-                    backgroundColor: '#e6e6e6',
+                    backgroundColor: '#f5f5f5',
                     justifyContent: 'flex-start',
-                    height: 180
+                    height: 100
                 }}>
                     <TouchableHighlight>
-                        <View style={styles.bottomCallButton}>
-                            <Text style={styles.bottomCallText}>还款咨询电话10108818</Text>
+                        <View style={styles.bottomCallButtonContainer}>
+                            <Image
+                                source={require('../assets/images/repay_call_center.png')}
+                                style={styles.bottomCallButtonImage}/>
+                            <View style={styles.bottomCallButton}>
+                                <Text style={styles.bottomCallText}>还款咨询电话10108818</Text>
+                            </View>
                         </View>
+
                     </TouchableHighlight>
                     <TouchableHighlight>
                         <View style={styles.bottomButton}>
                             <Text style={styles.bottomText}>确认还款2700.00元</Text>
                         </View>
                     </TouchableHighlight>
-
                 </View>
             </SafeAreaView>
 
@@ -236,12 +288,26 @@ const styles = StyleSheet.create({
         textAlignVertical: 'center',
         textAlign: 'center'
     },
+    bottomCallButtonContainer: {
+        alignSelf: 'center',
+        flexDirection: 'row',
+        borderColor: '#E6E6E6E6',
+        borderWidth: 1,
+        marginBottom: 15
+    },
+    bottomCallButtonImage: {
+        marginLeft: 15,
+        marginTop: 7
+    },
     bottomCallButton: {
-        alignItems: 'center',
+        alignSelf: 'center',
         height: 30,
-        borderColor: 'gray',
+        flexDirection: 'column',
+        justifyContent: 'center'
     },
     bottomCallText: {
+        marginLeft: 8,
+        marginRight: 15,
         color: 'gray',
         fontSize: 12,
         textAlignVertical: 'center',
